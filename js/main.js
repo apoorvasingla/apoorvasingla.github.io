3218db5b7d87a38278df0eec3e4a71ee06987ebf
Arpit Singla <arpit270896@gmail.com>
2016-05-27 01:59:09 +0530

    Completely refactor main.js
    Shittiest javascript ever

--- js/main.js
+++ js/main.js
@@ -1,6 +1,3 @@
-{
-    transition_delay: 1000
-}
 /*!
  * Start Bootstrap - Agnecy Bootstrap Theme (http://startbootstrap.com)
  * Code licensed under the Apache License v2.0.
@@ -9,8 +6,84 @@
 
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 
-var pageScrollClicked = false;
-$(function() {
+navbar = {
+    hide: function() {
+        $('nav.navbar').addClass("hide-navbar");
+    },
+    show: function() {
+        $('nav.navbar').removeClass("hide-navbar");
+    },
+    menu: {
+        hide: function() {
+            $('.navbar-collapse').collapse('hide');
+        },
+        show: function() {
+            $('.navbar-collapse').collapse('show');
+        }
+    }
+}
+
+
+animations = {
+    skillSlider: function(){
+        $('.all-skills').owlCarousel({
+            slideSpeed: 400,
+             itemsCustom: [
+                 [0, 4],
+                 [400, 4],
+                 [500, 5],
+                 [620, 6],
+                 [700, 8],
+                 [992, 5],
+                 [1200, 6]
+             ],
+        });
+
+        var sklData = $('.all-skills').data('owlCarousel');
+
+        var sklTgt = $('.nav-btn').find('.go');
+        sklTgt.on('click', function(e){
+            e.preventDefault();
+            if( $(this).hasClass('go-left') ) {
+                sklData.prev();
+            } else {
+                sklData.next();
+            }
+        });
+    },
+
+    scrollSpy: function() {
+        // Highlight the top nav as scrolling occurs
+        $('body').scrollspy({
+            target: '.navbar-fixed-top'
+        });
+    },
+
+    wow: function() {
+        // Wow js to load projects
+        new WOW({
+            offset: 200,
+            mobile: false
+        }).init();
+    },
+
+    waypoint: function() {
+        // Progress bar filler
+        $('.skills-box').waypoint({
+            handler: function(event, direction) {
+                $('.progress .progress-bar').progressbar({});
+            },
+            offset: '60%'
+        });
+    },
+
+    niceScroll: function() {
+        // Nicescroll plugin
+        $("html").niceScroll();
+    }
+}
+
+bindPageScrollClick = function() {
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
@@ -19,92 +92,47 @@ $(function() {
         pageScrollClicked = true;
         setTimeout(function(){
           pageScrollClicked = false;
-            $('nav.navbar').addClass("hide-navbar");
+            navbar.hide();
         }, 1500);
         event.preventDefault();
     });
-});
+}
 
-// Highlight the top nav as scrolling occurs
-$('body').scrollspy({
-    target: '.navbar-fixed-top'
-})
 
-// Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a').click(function() {
-    $('.navbar-toggle:visible').click();
-});
-
-$('div.modal').on('show.bs.modal', function() {
-    var modal = this;
-    var hash = modal.id;
-    window.location.hash = hash;
-    window.onhashchange = function() {
-        if (!location.hash){
-            $(modal).modal('hide');
+bindUpDownScroll = function(){
+    $(window).scroll(function(event){
+        var st = $(this).scrollTop();
+        if (pageScrollClicked == false) {
+            navbar.menu.hide();
+            if (st > lastScrollTop){
+                navbar.hide();
+            } else {
+                navbar.show();
+            }
         }
-    }
-});
-
-$('.skills-box').waypoint({
-    handler: function(event, direction) {
-        $('.progress .progress-bar').progressbar({});
-    },
-    offset: '60%'
-});
-
-$(document).ready(function($){
-    $('.all-skills').owlCarousel({
-        slideSpeed: 400,
-         itemsCustom: [
-             [0, 4],
-             [400, 4],
-             [500, 5],
-             [620, 6],
-             [700, 8],
-             [992, 5],
-             [1200, 6]
-         ],
+        lastScrollTop = st;
     });
+}
 
-    var sklData = $('.all-skills').data('owlCarousel');
-
-    var sklTgt = $('.nav-btn').find('.go');
-    sklTgt.on('click', function(e){
-        e.preventDefault();
-        if( $(this).hasClass('go-left') ) {
-            sklData.prev();
-        } else {
-            sklData.next();
-        }
+bindNavMenuClick = function(){
+    $('.navbar-collapse ul li a').click(function() {
+        navbar.menu.hide();
     });
-});
+}
 
-$(document).ready(
-    function() {
-        $("html").niceScroll();
+$('document').ready(function() {
+
+    // Load all animations
+    for(var animation in animations) {
+        eval("animations." + animation + "()");
     }
-);
 
-var lastScrollTop = 0;
-$(window).scroll(function(event){
-    var st = $(this).scrollTop();
-    if (st > lastScrollTop){
-        $('.navbar-toggle:visible').collapse();
-        $('nav.navbar').addClass("hide-navbar");
-    } else {
-    if (pageScrollClicked == false){
-            $('nav.navbar').removeClass("hide-navbar");
-        }
-     }
-    lastScrollTop = st;
+    pageScrollClicked = false;
+    bindPageScrollClick();
+
+    lastScrollTop = 0;
+    bindUpDownScroll();
+
+    bindNavMenuClick();
+
 });
-
-$('document').ready(
-    function() {
-    new WOW({
-        offset: 200,
-        mobile: false
-    }).init();
-    }
-);
