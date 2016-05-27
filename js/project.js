1128536db66aa195d1380e66d5276c9e9d871c27
Arpit Singla <arpit270896@gmail.com>
2016-05-28 00:59:14 +0530

    Adds library for image gallery

--- js/project.js
+++ js/project.js
@@ -1,3 +1,11 @@
+function isMobile() {
+    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+}
+
 document.addEventListener("DOMContentLoaded",function(){
-    $('.about-project').sticky();
+    $('.project-image a').fluidbox({});
+    if(!isMobile()) {
+        $('.about-project').sticky();
+    }
 });
+
