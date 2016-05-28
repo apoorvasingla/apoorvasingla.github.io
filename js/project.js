bd715de726dfbb19710e2ee995e6915ed82e2d5e
Arpit Singla <arpit270896@gmail.com>
2016-05-28 12:07:20 +0530

    Completely changes _includes directory structure

--- js/project.js
+++ js/project.js
@@ -2,8 +2,8 @@ function isMobile() {
     return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
 }
 
-document.addEventListener("DOMContentLoaded",function(){
-    $('.project-image a').fluidbox({});
+$(document).ready(function(){
+    $('.project-image a').fluidbox();
     if(!isMobile()) {
         $('.about-project').sticky();
     }
